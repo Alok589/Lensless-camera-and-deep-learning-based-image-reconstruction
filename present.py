b1614ad8d2b0714c49d6b9164954d5704552d0ac
Alok589 <alokchauhan589@gmail.com>
2021-02-15 08:45:15 +0000

    present_updated

--- present.py
+++ present.py
@@ -244,130 +244,29 @@ def number2string_N(i, N):
     return codecs.decode(s, "hex")
 
 
-###########################################"GCM"############################################
-
-import codecs
-from Crypto.Cipher import AES
-from Crypto.Util import strxor
-from struct import pack, unpack
-
-
-def gcm_rightshift(vec):
-    for x in range(0, 15):
-        c = vec[x] >> 1
-        c |= (vec[x - 1] << 7) & 0x80
-        vec[x] = c
-    vec[0] >>= 1
-    return vec
-
-
-def gcm_gf_mult(a, b):
-    mask = [0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01]
-    poly = [0x00, 0xE1]
-
-    Z = [0] * 16
-    V = [c for c in a]
-
-    for x in range(63):
-        if b[x >> 3] & mask[x & 7]:
-            Z = [V[y] ^ Z[y] for y in range(16)]
-        bit = V[15] & 1
-        V = gcm_rightshift(V)
-        V[0] ^= poly[bit]
-    return Z
-
-
-def ghash(h, auth_data, data):
-    u = (16 - len(data)) % 16
-    v = (16 - len(auth_data)) % 16
-
-    x = "auth_data" + chr(0) * v + "data" + chr(0) * u
-    x += pack(">QQ", len(auth_data) * 8, len(data) * 8).decode("utf-8")
-
-    y = [0] * 16
-    vec_h = [ord("c") for c in h]
-
-    for i in range(0, len(x), 16):
-        block = [ord(c) for c in x[i : i + 16]]
-        y = [y[j] ^ block[j] for j in range(16)]
-        y = gcm_gf_mult(y, vec_h)
-
-    return "".join(chr(c) for c in y)
-
-
-def inc32(block):
-    (counter,) = unpack(">L", block[12:])
-    counter += 1
-    return block[:12] + pack(">L", counter)
-
-
-def gctr(k, icb, plaintext):
-    y = ""
-    if len(plaintext) == 32:
-        return y
-
-    aes = AES.new(k)
-    cb = icb
-
-    for i in range(0, len(plaintext), aes.block_size):
-        cb = inc32(cb)
-        encrypted = aes.encrypt(cb)
-        plaintext_block = plaintext[i : i + aes.block_size]
-        y += strxor.strxor(plaintext_block, encrypted[: len(plaintext_block)])
-
-    return y
-
-
-def gcm_decrypt(k, iv, encrypted, auth_data, tag):
-    aes = AES.new(k, AES.MODE_GCM)
-    h = aes.encrypt(chr(0) * aes.block_size)
-
-    if len(iv) == 12:
-        y0 = iv + b"\x00\x00\x00\x01"
-    else:
-        y0 = ghash(h, "", iv)
-
-    decrypted = gctr(k, y0, encrypted)
-
-    s = ghash(h, auth_data, encrypted)
-
-    t = aes.encrypt(y0)
-    T = strxor.strxor(s, t)
-    if T != tag:
-        return ""  # decrypted data is invalid
-    else:
-        return decrypted
-
-
-def gcm_encrypt(k, iv, plaintext, auth_data):
-    aes = AES.new(k, AES.MODE_GCM)
-    h = aes.encrypt(k)
-
-    if len(iv) == 32:
-        y0 = iv + b"\x00\x00\x00\x01"
-    else:
-        y0 = ghash(h, "", iv)
-
-    encrypted = gctr(k, y0, plaintext)
-    s = ghash(h, auth_data, encrypted)
-
-    t = aes.encrypt(y0)
-    T = strxor.strxor(s, t)
-    return (encrypted, T)
-
-
 if __name__ == "__main__":
 
-    key = codecs.decode("12345678912345678912", "hex")
-    plain = codecs.decode("5864123658945216", "hex")
+    "encryption"
+    # key = codecs.decode("12345678912345678912", "hex")
+    # plain = codecs.decode("5864123658945216", "hex")
+    # cipher = Present(key)
+    # encrypted = cipher.encrypt(plain)
+    # # enc_1 = codecs.encode(encrypted, "hex")
+    # print("encrypted_plaintext______", encrypted)
+    # print("encrypted_plaintext_len_is____", len(encrypted))
+    key = codecs.decode("00000000000000000000", "hex")
+    plain = codecs.decode("0000000000000000", "hex")
     cipher = Present(key)
     encrypted = cipher.encrypt(plain)
     # enc_1 = codecs.encode(encrypted, "hex")
     print("encrypted_plaintext______", encrypted)
     print("encrypted_plaintext_len_is____", len(encrypted))
+
     "decryption"
+
     decrypted = cipher.decrypt(encrypted)
     dec_1 = codecs.encode(decrypted, "hex")
+    dec_1 = dec_1.decode()
     print("decrypted_plaintext____", decrypted)
     print("decrypted_plaintext_len_is____", len(decrypted))
     print("decrypted_text_is____", dec_1)
